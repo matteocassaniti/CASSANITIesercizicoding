c174293c53ccb16e2ce403d1ded3eab634da843c
matteocassaniti <169179506+matteocassaniti@users.noreply.github.com>
2025-10-27 12:58:59 +0100

    Add files via upload

---
+++ Cassanitipy1.py
@@ -0,0 +1,7 @@
+x = float(input("Insert any number: "))
+y = float(input("Insert any number: "))
+                  
+print("Somma: ", x + y) 
+print("Differenza: ", x - y) 
+print("Moltiplicazone: ", x * y) 
+print("Quoziente: ", x / y) 
