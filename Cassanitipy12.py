b66247e5d6c9785d912048c83e11c5ed51b495ae
matteocassaniti <169179506+matteocassaniti@users.noreply.github.com>
2025-12-01 13:10:59 +0100

    Add files via upload

---
+++ Cassanitipy12.py
@@ -0,0 +1,11 @@
+class auto:
+
+    def __init__(self,proprietario):
+
+        self.proprietario = proprietario 
+
+    def scheda (self):
+        return f'\nScheda '
+    
+matteo = auto 
+gio = "CHEDDIRA", 215674674
